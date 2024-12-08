4ea107e00c54f4c2c897d38a49bc05d2de29be8e
Afnan Ahmad Tariq <116060169+afnanahmadtariq@users.noreply.github.com>
2024-12-08 07:00:13 +0500

    v0.6.1
    fix user id

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,20 +4,12 @@ const { neon } = require("@neondatabase/serverless");
 const sql = neon(process.env.DATABASE_URL);
 
 exports.createUser = async (req, res) => {
-  const { user_id, username, email, password, bio, profile_pic, expertise, social_links } = req.body;
-
-  const query = `
-    INSERT INTO "User" ("user_id", "username", "email", "password", "bio", "profile_pic", "expertise", "social_links")
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    RETURNING "user_id";
-  `;
-
-  const values = [user_id, username, email, password, bio, profile_pic, expertise, social_links];
+  const { username, email, password, bio, profile_pic, expertise, social_links } = req.body;
 
   try {
     const result = await sql`
-      INSERT INTO "User" ("user_id", "username", "email", "password", "bio", "profile_pic", "expertise", "social_links")
-      VALUES (${user_id}, ${username}, ${email}, ${password}, ${bio}, ${profile_pic}, ${expertise}, ${social_links})
+      INSERT INTO "User" ("username", "email", "password", "bio", "profile_pic", "expertise", "social_links")
+      VALUES (${username}, ${email}, ${password}, ${bio}, ${profile_pic}, ${expertise}, ${social_links})
       RETURNING "user_id";
     `;
     res.status(201).json({
