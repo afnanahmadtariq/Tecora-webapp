f3c36a70f2dc2b98b689f6b77c5b7d1da64ea0eb
Coding Syed <148377982+aligee12@users.noreply.github.com>
2024-12-10 21:03:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const morgan = require('morgan');
-const express = require('express');
-const cors = require('cors');
-const postRoutes = require('./routes/posts');
-const queries = require('./controllers/myQueries');  
-const projects = require('./controllers/projects'); 
-const createRoutes = require('./routes/createUser');
-const updateRoutes = require('./routes/updateUser');
-const queriesRoutes = require('./routes/queries');
-const loginRoutes = require('./routes/login');
+const morgan = require("morgan");
+const express = require("express");
+const cors = require("cors");
+const postRoutes = require("./routes/posts");
+const queries = require("./controllers/myQueries");
+const projects = require("./controllers/projects");
+const createRoutes = require("./routes/createUser");
+const updateRoutes = require("./routes/updateUser");
+const queriesRoutes = require("./routes/queries");
+const loginRoutes = require("./routes/login");
 const query1 = require("./controllers/myQueries");
 
 const app = express();
@@ -16,29 +16,29 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json()); // Replaced body-parser with express.json()
-app.use(morgan('dev')); // Add this line for logging
+app.use(morgan("dev")); // Add this line for logging
 
 // Routes
-app.use('/api/posts', postRoutes);
+app.use("/api/posts", postRoutes);
 
-app.use('/api/create-user', createRoutes);
-app.use('/api/update-user', updateRoutes);
-app.use('/api/login', loginRoutes);
+app.use("/api/create-user", createRoutes); //change to /api/users/create
+app.use("/api/update-user", updateRoutes);
+app.use("/api/login", loginRoutes);
 
 // Endpoint to get queries
-app.use('/api/queries', queriesRoutes);
+app.use("/api/queries", queriesRoutes);
 
-app.get('/api/projects', (req, res) => {
+app.get("/api/projects", (req, res) => {
   res.json(projects);
 });
 
-app.get('/api/queries/1', (req, res) => {
+app.get("/api/queries/1", (req, res) => {
   res.json(query1);
 });
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 // Start Server
